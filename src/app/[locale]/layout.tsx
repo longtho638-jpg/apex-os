813318ff179689861aac1ce3c002430ea6a45223
Người hướng dẫn <longtho638@gmail.com>
2025-11-22 09:59:54 +0700

    fix: remove generateStaticParams to allow dynamic SSR on Vercel

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,9 +3,7 @@ import { getMessages, setRequestLocale } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
 
-export function generateStaticParams() {
-    return routing.locales.map((locale) => ({ locale }));
-}
+
 
 export default async function LocaleLayout({
     children,
